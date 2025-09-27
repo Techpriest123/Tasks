c1877dc9e793054b573ad6f4360003b28ec9d5a1
Techwizrd <m9805552471@gmail.com>
2025-09-27 14:51:23 +0300

    initial commit

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("X: ");
+    float x = scan.nextFloat();
+    System.out.println("Y: ");
+    float y = scan.nextFloat();
+
+    if (x > 0 && y > 0)
+      System.out.println("Quad 1");
+    if (x < 0 && y > 0)
+      System.out.println("Quad 2");
+    if (x < 0 && y < 0)
+      System.out.println("Quad 3");
+    if (x > 0 && y < 0)
+      System.out.println("Quad 4");
+
+    if (x == 0 && y != 0)
+      System.out.println("Axis Y");
+    if (y == 0 && x != 0)
+      System.out.println("Axis X");
+    scan.close();
+    if (x == 0 && y == 0)
+      System.out.println("Center");
+  }
+}
